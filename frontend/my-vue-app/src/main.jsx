8d6c2fe8863bad84cc5b845a90ca8b555e3f602c
VictorCosme <victorcosmemelo@gmail.com>
2022-11-06 17:09:09 -0300

    Configura as rotas: pronto para add paginas

--- frontend/my-vue-app/src/main.jsx
+++ frontend/my-vue-app/src/main.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import "./index.css";
+
+import Home from './pages/Home/index.jsx'
+import Login from './pages/Login/index.jsx'
+import Signup from './pages/Signup/index.jsx'
+import Profile from './pages/Profile/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login/>,
+  },
+  {
+    path: "/home",
+    element: <Home/>
+  },
+  {
+    path: "/signup",
+    element: <Signup/>
+  },
+  {
+    path: "/profile",
+    element: <Profile/>
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
